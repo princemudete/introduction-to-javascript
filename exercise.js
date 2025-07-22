4c2b1a34ce0dc5b8895e0cd1320aef7110bccc13
princemudete <princemudete@gmail.com>
2025-07-22 11:38:47 +0200

    Updated layout for wrapper and nav

---
+++ exercise.js
@@ -0,0 +1,10 @@
+let score = parseInt(prompt("Enter your score"))
+if (score >= 90){
+    alert("Grade: A")
+}else if (score >= 80){
+    alert("Grade: B")
+}else if (score >=70){
+   alert("Grade: C")
+}else if (score >= 60){
+    alert("Grade: D")
+}
