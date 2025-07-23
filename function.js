34c0ba44ecb2ea59845b13fd12e7028c601cb7ed
princemudete <princemudete@gmail.com>
2025-07-23 11:38:01 +0200

    Updated layout for wrapper and nav

---
+++ function.js
@@ -0,0 +1,20 @@
+function greet() {
+    console.log("Hello, World!");
+}
+greet();
+
+function say() {
+    console.log("Hi, Prince!");        
+}
+say();
+
+function add(a, b) {
+    console.log(10 + 20);
+}
+add();
+
+function multiply(f, g) {
+    return f * g;
+    
+}
+console.log(multiply(5, 20));
